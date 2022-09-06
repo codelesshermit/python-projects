7ebdac4a23745e2cf897c40380025f9584751ea2
Codelesshermit <anto100charles@gmail.com>
2022-09-06 21:15:22 +0300

    started working on digital beggar. made a simple interface

--- digital beggar/app.py
+++ digital beggar/app.py
@@ -1,1 +1,23 @@
-print("Hello World")
+import PySimpleGUI as sg
+import qrcode
+
+layout = [
+    [sg.Text('E-Beg Platform', key='-HEADING-')],
+    [sg.Button('Contribute', key='-CONTRIBUTE-'), sg. Button('Offer Job or Other Help', key='-OTHER-')],
+    [sg.HSeparator()],
+    [sg.Text('Property of DigitaL Beggar. Ts & Cs Apply')]
+]
+window = sg.Window("Digital Beggar", layout)
+
+while True:
+    event, values = window. read()
+
+    if event == '-CONTRIBUTE-':
+        qr = qrcode.make("We are working on the payment systems")
+        qr.save('PAYMENT.PNG')
+
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
+
