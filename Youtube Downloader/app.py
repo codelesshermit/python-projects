c1aff710101792cf75a355f21418f73a1e6c33df
Codelesshermit <anto100charles@gmail.com>
2022-10-30 12:47:43 +0300

    created a simple youtube downloader app

---
+++ Youtube Downloader/app.py
@@ -0,0 +1,66 @@
+import PySimpleGUI as sg
+from pytube import YouTube
+import os
+from pathlib import Path
+from PIL import Image
+import requests
+
+
+
+
+def download_audio(link):
+    # download path
+    folder = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Downloads/Youtube Download')
+    yt = YouTube(link)
+    yt.streams.get_audio_only().download(output_path=folder)
+
+def download_video(link):
+    #download path
+    path_to_download_folder = str(os.path.join(Path.home(), "Downloads/Youtube Download"))
+    yt = YouTube(link,)
+    yt.streams.get_highest_resolution().download(output_path=path_to_download_folder)
+
+def create_Window():
+    pass
+sg.theme('SystemDefault1')
+layout = [
+            [sg.Text('Paste the youtube Link below ', key='-TEXT-')],
+            [sg.Input(key='-LINK-'), sg.Button('Preview', key='-PREVIEW-')],
+            [sg.Text('', key='-TITLE-')],
+            [sg.Image('', key='-THUMBNAIL-', size=(250,250), visible=False)],
+            [sg.Button('Download Audio', key='-AUDIO-', visible=False), sg.Button('Download Video', key='-VIDEO-', visible=False)]
+         ]
+
+window = sg.Window('Youtube Video/Audio Downloader', layout, size=(500, 500))
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    if event == '-PREVIEW-':
+        yt = YouTube(values['-LINK-'])
+        print(yt.title)
+        window['-TITLE-'].update(yt.title)
+        im = Image.open(requests.get(yt.thumbnail_url, stream=True).raw)
+        im = im.save('image.png')
+        #sg.Image('image.png', size=(300,300))
+        window['-THUMBNAIL-'].update(visible=True)
+        window['-THUMBNAIL-'].update('image.png')
+        window['-AUDIO-'].update(visible=True)
+        window['-VIDEO-'].update(visible=True)
+    
+    if event == '-AUDIO-':
+        download_audio(values['-LINK-'])
+        sg.Popup("Your Audio has been downloaded.\n Check Downloads\\Youtube Download ")
+        window.refresh()
+
+    if event == '-VIDEO-':
+        download_video(values['-LINK-'])
+        sg.Popup("Your Video has been downloaded.\n Check Downloads\\Youtube Download ")
+        window.refresh()
+
+
+
+window.close()
